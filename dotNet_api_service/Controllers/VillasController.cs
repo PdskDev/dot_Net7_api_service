deb8cee7d8fd2426128e42882560cfc74f2626ee
PdskDev <prestadesk@gmail.com>
2023-07-18 20:36:55 +0200

    Init commit - First Controller

---
+++ dotNet_api_service/Controllers/VillasController.cs
@@ -0,0 +1,25 @@
+﻿using dotNet_api_service.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet_api_service.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/villas")]
+    [ApiController]
+    public class VillasController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> getVillas()
+        {
+            return new List<Villa>
+            {
+               new Villa{Id=1, Name="SeaSide Villa"},
+               new Villa{Id=2, Name="NearForest Villa"},
+               new Villa{Id=3, Name="Sunrise Temple Villa"},
+               new Villa{Id=4, Name="Moonlight Elegant Palace"},
+               new Villa{Id=5, Name="BellaPlaza Villa"},
+               new Villa{Id=6, Name="Paradise Residence Villa"}
+            };
+        }
+    }
+}
---
+++ dotNet_api_service/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace dotNet_api_service.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
